57c95b15dbfdcd70f5405719b5e1cc93c1cbb455
Priyanshu4832 <randomstuff4browser@gmail.com>
2025-07-26 12:30:36 +0530

    Time: 15 ms (39.73%) | Memory: 11.3 MB (94.96%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -12,17 +12,26 @@ public:
         int cnt =0;
 
         while(r<s.size()){
+
+            //char is availabe in my hash so decrease its freqncy cnt will increase only when its freq is more than 0 
+
+            // if freq is negative that means we are finding extra of those and in our current window required amount i already there 
             if(hash.find(s[r])!=hash.end()){
                 if(hash[s[r]]>0){
                     cnt+=1;
                 }
                 hash[s[r]]--;
 
+                //when cnt == required now shrink the window 
+                
+
                 while(cnt==t.size()){
+                    //update lenght and starting idx
                     if(r-l+1<minLen){
                         startIdx = l;
                         minLen = r-l+1;
                     }
+                    // cnt will dcrease only when its freq goes above 0 that means our current windoes does not have sufficent amount of that character
                     if(hash.find(s[l])!=hash.end()){
                         hash[s[l]]++;
                         if(hash[s[l]]>0) cnt--;
