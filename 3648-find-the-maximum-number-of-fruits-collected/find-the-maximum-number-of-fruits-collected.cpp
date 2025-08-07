8847eef92d02b2c7d68d4abcfb3cd0128cd2db22
Priyanshu4832 <randomstuff4browser@gmail.com>
2025-08-07 11:24:50 +0530

    Time: 73 ms (59.09%) | Memory: 173.6 MB (40.26%) - LeetSync

--- 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.cpp
+++ 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.cpp
@@ -68,9 +68,14 @@ public:
         collect+=func1(0,n-1,fruits,dp);
 
 
-        vector<vector<long long>> dpp(n,vector<long long>(n,-1));
+        for(int i=0 ; i<n ; i++){
+            for(int j=0 ; j<n ; j++){
+                dp[i][j]=-1;
+            }
+        }
         
-        collect+=func2(n-1,0,fruits,dpp);
+        
+        collect+=func2(n-1,0,fruits,dp);
 
 
 
