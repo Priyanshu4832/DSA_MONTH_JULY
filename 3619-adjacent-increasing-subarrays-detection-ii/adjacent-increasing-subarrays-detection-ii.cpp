ceca89e3058fc2766078b208c5573140e4beb13d
Priyanshu4832 <randomstuff4browser@gmail.com>
2025-10-15 18:46:27 +0530

    Time: 262 ms (16.54%) | Memory: 173.6 MB (89.47%) - LeetSync

--- 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.cpp
+++ 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.cpp
@@ -10,38 +10,38 @@ public:
 
         while(k<nums.size()){
             
-            //get the increasing sequenece 
+          
             int cnt1=0,cnt2=0;
-            //cout<<"element is "<<nums[k]<<" cnt1 "<<cnt1<<" k "<<k<<endl;
+          
             while( k+1<n && nums[k]<nums[k+1]){
-                //cout<<"element is "<<nums[k]<<" cnt1 "<<cnt1<<" k "<<k<<endl;
+               
                 cnt1++;
                 k++;
-                //cout<<"element is "<<nums[k]<<" cnt1 "<<cnt1<<" k "<<k<<endl;
+               
                 
             }
             cnt1++;
             k++;
-            //cout<<"element is "<<nums[k]<<" cnt1 "<<cnt1<<" k "<<k<<endl;
+            
 
             maxLen=max(maxLen , cnt1/2);
             maxLen=max(min(glob,cnt1) ,maxLen );
             cout<<maxLen<<endl;
 
-            //cout<<"element is "<<nums[k]<<" cnt2 "<<cnt2<<" k "<<k<<endl;
+           
             while( k+1<n && nums[k]<nums[k+1]){
-                //cout<<"element is "<<nums[k]<<" cnt2 "<<cnt2<<" k "<<k<<endl;
+                
                 cnt2++;
                 k++;
-                //cout<<"element is "<<nums[k]<<" cnt2 "<<cnt2<<" k "<<k<<endl;
+                
             }
             cnt2++;
             k++;
-            //cout<<"eelement is "<<nums[k]<<" cnt2 "<<cnt2<<" k "<<k<<endl;
+            
             maxLen = max(maxLen , cnt2/2);
             maxLen = max(maxLen , min(cnt1,cnt2));
             glob = cnt2;
-            cout<<maxLen<<endl;
+           
 
 
         }
