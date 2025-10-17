dd5c7eb7586ca2915eae906cc242b12bf437c80f
Priyanshu4832 <randomstuff4browser@gmail.com>
2025-10-17 18:32:51 +0530

    Time: 131 ms (5.85%) | Memory: 31 MB (61.97%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
-    int func(int i , int j , vector<vector<char>>& matrix , vector<vector<int>>& down){
-        if(i>=matrix.size()) return 0;
 
-        if(down[i][j]!=-1) return down[i][j];
-       
-        if(matrix[i][j]=='1') {
-            return down[i][j]= 1+func(i+1,j,matrix,down);
-        }
-        return down[i][j]=0;
-    }
     int maximalSquare(vector<vector<char>>& matrix) {
         
         int m = matrix.size();
@@ -17,13 +8,26 @@ public:
         int flag=0;
  
 
-        vector<vector<int>> down(m,vector<int>(n,-1));
+        vector<vector<int>> down(m,vector<int>(n,0));
 
 
-        for(int i=0 ; i<m ; i++){
-            for(int j=0 ; j<n ; j++){
+        for(int i=m-1 ; i>=0 ; i--){
+            for(int j=n-1 ; j>=0 ; j--){
                 if(flag==0 && matrix[i][j]=='1') flag=1;
-                func(i,j,matrix,down);
+
+                if(i==m-1){
+                    if(matrix[i][j]=='1') down[i][j]=1;
+                    else down[i][j]=0;
+                }
+                else{
+                    if(matrix[i][j]=='1') down[i][j]=1+down[i+1][j];
+                    else down[i][j]=0;
+                }
+                
+                
+
+                
+             
             }
         }
 
@@ -37,7 +41,7 @@ public:
                 int downLen=down[i][j];
 
                 for(int k = j+1 ; k<n ; k++){
-                    if(matrix[i][k]!=0){
+                    if(matrix[i][k]=='1'){
                         downLen=min(downLen,down[i][k]);
                         if(downLen==k-j+1) maxSquare=max(downLen*downLen,maxSquare);
                     }
