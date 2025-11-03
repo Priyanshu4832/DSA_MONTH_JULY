56cb1b7974c988c0e418be3882b69f49a1f8df22
Priyanshu4832 <randomstuff4browser@gmail.com>
2025-11-03 23:22:13 +0530

    Time: 44 ms (64.29%) | Memory: 124.1 MB (99.97%) - LeetSync

--- 4101-maximum-product-of-three-elements-after-one-replacement/maximum-product-of-three-elements-after-one-replacement.cpp
+++ 4101-maximum-product-of-three-elements-after-one-replacement/maximum-product-of-three-elements-after-one-replacement.cpp
@@ -7,7 +7,16 @@ public:
         long long res;
         for(int i=0 ; i<n ; i++) nums[i]=abs(nums[i]);
         sort(nums.begin() , nums.end());
-        res = abs(1e5*nums[n-1]*nums[n-2]);
+        int max1=nums[1];
+        int max2=nums[0];
+        for(int i=0 ; i<n ; i++){
+            if(nums[i]>=max1){
+                max2=max1;
+                max1=nums[i];
+                
+            }
+        }
+        res = abs(1e5*max1*max2);
         return res;
 
 
