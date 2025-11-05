55954dacbb0c8d929a415341fde31d5ec7758a9c
Priyanshu4832 <randomstuff4browser@gmail.com>
2025-11-05 14:08:39 +0530

    Time: 0 ms (100.00%) | Memory: 112.3 MB (84.79%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -1,21 +1,5 @@
 class Solution {
 public:
-    bool check(int idx ,vector<int> & deficet){
-
-        int n = deficet.size();
-        
-
-        int fuel=0;
-        for(int i=idx ; i<n ; i++){
-            fuel+=deficet[i];
-            if(fuel<0) return false;
-        }
-        for(int i=0 ; i<idx ; i++){
-            fuel+=deficet[i];
-            if(fuel<0) return false;
-        }
-        return true;
-    }
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         
 
@@ -30,7 +14,7 @@ public:
         for(int i=0 ; i<n; i++){
             int cal=gas[i]-cost[i];
 
-            // deficet[i]=cal;
+  
             cnt+=cal;
             fuel+=cal;
             if(fuel<0){
