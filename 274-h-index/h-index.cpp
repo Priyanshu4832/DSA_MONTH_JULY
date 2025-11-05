6179f08648d1c309f767058cab56436fe9dfbb15
Priyanshu4832 <randomstuff4browser@gmail.com>
2025-11-05 12:52:01 +0530

    Time: 31 ms (5.06%) | Memory: 12.4 MB (37.78%) - LeetSync

---
+++ 274-h-index/h-index.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        
+        sort(citations.begin() , citations.end());
+
+        int h=0;
+        for(int i=0 ; i<citations.size() ; i++){
+            int cnt = citations[i];
+            int paperCnt = citations.size()-i;
+            cout<<"citations "<<cnt<<" papers "<<paperCnt<<endl;
+            if(cnt>=paperCnt){
+                h=max(h,paperCnt);
+            }
+        }
+        return h;
+    }
+};
