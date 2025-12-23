96584b3c762e01d4f8f44f243ec47fb7ce45bf7f
Priyanshu4832 <randomstuff4browser@gmail.com>
2025-12-24 00:41:32 +0530

    Time: 4 ms (25.90%) | Memory: 107 MB (80.57%) - LeetSync

--- 2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.cpp
+++ 2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.cpp
@@ -1,8 +1,36 @@
+// class Solution {
+// public:
+//     long long getDescentPeriods(vector<int>& prices) {
+        
+//         long long ans = 0 ;
+
+//         int l=0;
+//         int r = 0;
+
+//         vector<int> breaks;
+//         int len = 1;
+
+
+//         while(r<prices.size()){
+            
+//             if(r!=0){
+//                 if(prices[r]+1!=prices[r-1]){
+//                     l=r;
+//                 }
+//             }
+//             ans+=(r-l+1);
+//             r++;
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     long long getDescentPeriods(vector<int>& prices) {
         
         long long ans = 0 ;
+        int s = prices.size();
 
         int l=0;
         int r = 0;
@@ -11,16 +39,20 @@ public:
         int len = 1;
 
 
-        while(r<prices.size()){
+        while(r<s){
             
             if(r!=0){
                 if(prices[r]+1!=prices[r-1]){
+                    int n = (r-l);
+                    ans+= 1LL*n*(n+1)/2;
                     l=r;
                 }
             }
-            ans+=(r-l+1);
+            //ans+=(r-l+1);
             r++;
         }
+        int n = (r-l);
+        ans+= 1LL*n*(n+1)/2;
         return ans;
     }
 };
