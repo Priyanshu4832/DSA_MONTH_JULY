8cbaff1599ed90132d79fe780a6d376c0f8778eb
Priyanshu4832 <randomstuff4browser@gmail.com>
2026-02-02 10:39:10 +0530

    Time: 441 ms (45.31%) | Memory: 182.8 MB (31.25%) - LeetSync

---
+++ 3260-divide-an-array-into-subarrays-with-minimum-cost-ii/divide-an-array-into-subarrays-with-minimum-cost-ii.cpp
@@ -0,0 +1,85 @@
+class Solution {
+public:
+
+    multiset<int> L , R;
+    
+
+    void add(int val , int k, long long &currentSum){
+
+        L.insert(val);
+        currentSum +=val;
+
+        if(L.size() > k-1){
+            int largest_l = *L.rbegin();
+            L.erase(prev(L.end()));
+
+            currentSum -=largest_l;
+            R.insert(largest_l);
+        }
+
+        // if largest in l is greater than smaller in R
+        if(!L.empty() && !R.empty() && *L.rbegin() > *R.begin()){
+            int largest_l = *L.rbegin();
+            int smallest_r = *R.begin();
+
+            L.erase(prev(L.end()));
+            currentSum -=largest_l;
+            R.erase(R.begin());
+
+            R.insert(largest_l);
+            L.insert(smallest_r);
+            currentSum +=smallest_r;
+        }
+
+    }
+    void remove(int val, int k , long long &currentSum){
+
+        auto it = R.find(val);
+        if( it!=R.end() ){
+            R.erase(it);
+        }
+
+        else{
+            auto it_l = L.find(val);
+            if (it_l != L.end()) {
+                L.erase(it_l);
+                currentSum -= val;
+            }
+            if (L.size() < k-1 && !R.empty()) {
+                int smallest_R = *R.begin();
+                R.erase(R.begin());
+                L.insert(smallest_R);
+                currentSum += smallest_R;
+            }
+        }
+    }
+
+    long long minimumCost(vector<int>& nums, int k, int dist) {
+        
+        L.clear();
+        R.clear();
+        int left = 1 ;
+        int right = dist+1;
+        long long currentSum=0;
+        long long ans=LLONG_MAX;
+        int targetSize = k-1;
+
+        for(int i=left ; i<=right ; i++){
+            add(nums[i],k,currentSum);
+        }
+        ans = (currentSum);
+        
+        right++;
+        while(right<nums.size()){
+            
+            add(nums[right],k,currentSum);
+            remove(nums[left],k,currentSum);
+
+            ans = min(ans , currentSum);
+            left++;
+            right++;
+        }
+
+        return nums[0]+ans;
+    }
+};
