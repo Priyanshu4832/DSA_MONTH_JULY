985ba5a0f920918dd129beec65e7ec95c703b2aa
Priyanshu4832 <randomstuff4browser@gmail.com>
2026-02-06 21:12:44 +0530

    Time: 56 ms (15.99%) | Memory: 105 MB (86.63%) - LeetSync

--- 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.cpp
+++ 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.cpp
@@ -1,27 +1,46 @@
 class Solution {
 public:
-   
-    int minRemoval(vector<int>& nums, int k) {
 
-        sort(nums.begin(),nums.end());
-        int n = nums.size();
+    int func(long long atMost , vector<int>& nums , int i){
         
-        int cnt=1e9;
+        int left = i+1;
+        int right = nums.size()-1;
 
-        int j = 0;
+        int ans = -1;
+        while(left<=right){
+            int mid = (left+right)/2;
 
-        for(int i=0 ; i<n ; i++){
-
-            while(j<n ){
-                long long cal = (long long)nums[i]*k;
-                if(cal>=nums[j]){
-                    cnt = min ( cnt , n - (j-i+1));
-                j++;
-                }
-                else break;
-                
+            if(nums[mid]<=atMost){
+                ans = mid;
+                left = mid+1;
             }
+            else right = mid-1;
         }
-        return cnt;
+        return ans;
+    }
+    int minRemoval(vector<int>& nums, int k) {
+        
+        sort(nums.begin() , nums.end());
+
+        int ans = 1e6;
+        int n = nums.size();
+        //for(auto it : nums) cout<<it<<" ";
+        //cout<<endl;
+
+        if(1LL*nums[0]*k>=nums[n-1]) return 0;
+
+        for(int i=0 ; i<nums.size()-1 ; i++){
+
+            long long atMost = 1LL*nums[i]*k;
+
+            int idx = func(atMost , nums , i);
+            if(idx==-1) continue;
+            //cout<<"i:"<<i<<" nums[i]:"<<nums[i]<<" idx:"<<idx<<" nums[idx]:"<<nums[idx]<<endl;
+            
+
+            ans = min(ans , i + (n-idx-1) );
+        }
+        if(ans==1e6) return n-1;
+        return ans;
     }
 };
