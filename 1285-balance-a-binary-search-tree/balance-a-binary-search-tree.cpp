61375be8bd3c48fcb75002b161a9f71af5652975
Priyanshu4832 <randomstuff4browser@gmail.com>
2026-02-09 10:36:42 +0530

    Time: 531 ms (12.73%) | Memory: 549 MB (12.54%) - LeetSync

---
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    //map<int,TreeNode*> mpp;
+    vector<int> v;
+
+    void trav(TreeNode* root){
+        if(root==NULL) return;
+
+        trav(root->left);
+        //mpp[root->val]=root;
+        v.push_back(root->val);
+        trav(root->right);
+    }
+
+
+    TreeNode* func(int l , int r , vector<int> arr){
+
+        if(l==r) return new TreeNode(arr[l]);
+        if(l>r) return NULL;
+
+        int mid = (l+r)/2;
+        
+
+        TreeNode* root = new TreeNode(arr[mid]);
+        root->left = func(l,mid-1,arr);
+        root->right = func(mid+1,r,arr); 
+        return root;
+
+    }
+    TreeNode* balanceBST(TreeNode* root) {
+        
+        trav(root);
+        //for(auto it : mpp) cout<<it.first<<" ";
+        sort(v.begin(),v.end());
+        TreeNode* newRoot = func(0,v.size()-1 , v);
+        return newRoot;
+
+
+
+        
+    }
+};
